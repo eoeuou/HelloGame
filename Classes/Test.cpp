51386ca7a2d675faa0fec96cd8464eaf1e6bcbb4
王华 <wanghua273@qq.com>
2014-08-16 11:14:12 +0800

    汉语测试

---
+++ Classes/Test.cpp
@@ -0,0 +1,9 @@
+//
+//  Test.cpp
+//  HelloGame
+//
+//  Created by wanghua on 14-8-16.
+//
+//
+
+#include "Test.h"
---
+++ Classes/Test.h
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  HelloGame
+//
+//  Created by wanghua on 14-8-16.
+//
+//
+
+#ifndef __HelloGame__Test__
+#define __HelloGame__Test__
+
+#include <iostream>
+
+#endif /* defined(__HelloGame__Test__) */
